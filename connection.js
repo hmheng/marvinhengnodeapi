f86559b82b12f589d98e1afb1a7fb4063266037a
Marvin Heng <Marvin.Heng@avire-global.com>
2017-12-31 23:30:58 +0800

    17.01

--- connection.js
+++ connection.js
@@ -1,8 +1,8 @@
 var mysql=require('mysql');
 var connection= mysql.createPool({
-	host     : 'hmhengwpdb.cuad7lc0sw9c.us-east-1.rds.amazonaws.com',
-	user     : 'hmheng_admin',
-	password : 'Cubicurve7db',
+	host     : '',
+	user     : '',
+	password : '',
 	database : 'testapi'
 });
 module.exports=connection;
